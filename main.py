5ac2f8a72177e0d2cf5969c08c9582a03b3976b7
Eugene <eugene@eu-onikienko.(none)>
2014-10-04 18:13:35 +0300

    correct mistakes

--- main.py
+++ main.py
@@ -1,28 +1,82 @@
 import numpy as np
 
-#----------------------- Timescales initialization ---------------------------
-
-t_st    = 0.0
-t_en    = 200.0
-dt      = 0.001
-t       = arange( t_st, t_en, dt )
 
 #------------------------ Paramete initialization ----------------------------
 
+# Period of oscilation
+T 			= 3.0
 # Ferromagnetic frequency
-om_FM       =
+om_FM       = 2*np.pi /T
 # Interlayer interection
-d_om_int    =
+d_om_int    = 0.7 * 2*np.pi /T
 #  Gilbert damping
-a_G         =
+a_G         = 0.01
+# Current in units of critical
+I 			= 3.0
+
+
+#----------------------- Timescales initialization ---------------------------
+
+t_st    = 0.0
+t_en    = 30*T
+dt      = 0.001
+t       = np.arange( t_st, t_en, dt )
+
 
 #------------------------------ Initial values -------------------------------
 
-theta0  = 0.01*np.pi
-phi0    = 0.1*np.pi
-ang = np.array([ theta0, phi0, np.pi - theta0, phi0,
-                 0., 0., 0., 0.  ])
+theta0  = 0.1*np.pi
+theta1	= np.pi - theta0
+d_phi  	= np.pi
+angs0 	= np.array([ theta0, theta1, d_phi])
 
 
+#-----------------------------------------------------------------------------
+
+from rhs import RHS_safm_polar
+safm = RHS_safm_polar( om_FM, d_om_int, a_G, I )
+
+from scipy.integrate import odeint
+angs = odeint( safm, angs0, t )
 
 
+#-----------------------------------------------------------------------------
+# from plotters import draw_angls_3D
+# draw_angls_3D( angs )
+
+from matplotlib import pyplot as pp
+
+max2 = angs[:, 0].max()
+min2 = angs[:, 0].min()
+yticks2 = np.linspace(min2, max2, 5)
+yt_labels2 = [ '$'+str(round(num, 1))+'\pi$' for num in yticks2 / np.pi]
+	
+max0 = angs[:, 0].max()
+min0 = angs[:, 0].min()
+	
+max1 = angs[:, 1].max()
+min1 = angs[:, 1].min()
+	
+mi = min(min0, min1)
+ma = max(max0, max1)
+yticks = np.linspace(mi, ma, 5)
+yt_labels = [ '$'+str(round(num, 1))+'\pi$' for num in yticks / np.pi]
+	
+fig = pp.figure()
+pp.subplot(211)
+pp.plot(t, angs[:, 2], 'g', label='$\Delta\phi$')
+pp.xlabel(r'time')
+pp.ylabel(r'$\Delta\phi$')
+pp.yticks(yticks2, yt_labels2)
+pp.grid(True)
+
+pp.subplot(212)
+pp.plot(t, angs[:, 0], 'r', label='$M_1$')
+pp.plot(t, angs[:, 1], 'b', label='$M_2$')
+pp.xlabel(r'time')
+pp.ylabel(r'$\theta$')
+pp.yticks(yticks, yt_labels)
+pp.legend(loc=7)
+pp.grid(True)
+
+pp.show()
--- rhs.py
+++ rhs.py
@@ -1,13 +1,30 @@
+import numpy as np
+
 class RHS_safm_polar( object ):
     """ RHS function, which describe two vectors of syntetic AFM."""
     def __init__( self, om_fm, om_int, a, I ):
-        self.om_fm  = om_fm
-        self.om_int = om_int
-        self.a      = a
+        self.of  	= om_fm
+        self.oi 	= om_int
+        self.so 	= om_fm + om_int
+        self.aof	= a * om_fm
+        self.aoi	= a * om_int
+        self.aa     = ( a*a + 1. )**-1
         self.i      = I*a*( om_fm + om_int )
+        self.ai 	= a*self.i
 
     
     def __call__( self, angs, t ):
+    	""" angs = { theta0, theta1, d_phi } """
+    	s = np.sin( angs )
+    	c = np.cos( angs )
+
+    	return self.aa * np.array([
+    		self.oi*s[1]*s[2] + self.i*s[0] - self.aof*s[0]*c[0] + self.aoi*( s[0]*c[1] - c[0]*s[1]*c[2] ),
+    		-self.oi*s[0]*s[2] - self.i*s[1] - self.aof*s[1]*c[1] + self.aoi*( s[1]*c[0] - c[1]*s[0]*c[2] ),
+    		self.so*(c[0]-c[1]) + 2*self.ai + ( self.oi*c[2]*(c[0]*s[1]**2 - c[1]*s[0]**2) + self.aoi*s[2]*(s[0]**2 + s[1]**2) ) / (s[0]*s[1])
+    	])
+
+
         
         
         
